23eb936b1c2f27285b764b3674685fb51ba6b0d1
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-09-06 15:51:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,59 @@
+// This code activates express.js
 const express = require('express')
+
+// This makes it into the app variable below.
 const app = express()
+
+// Picking a port to operate through
 const port = 3000
 
+// Getting today's date using Date()
+const todays_date = new Date()
+
+// Variables for the movie list
+let favoriteMovieList = ['Fight Club', 'The Big Lebowski', 'Pulp Fiction', 'Old School'];
+let newMovie = null;
+let moviesString = favoriteMovieList.join(', ');
+
+// Global variables for getting the first and last name through the website
+let queryFirstName = null;
+let queryLastName = null;
+
+
+
+// *** All of the apps made using the different variables above
+
+// Printing name and date to the website
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send(`My name is Brennan Stopnik. Today's date is: ${todays_date}`);
 })
 
+// Getting the first and last names
+app.get('/get-user-info', (req, res) => {
+    console.log("req.query", req.query);
+    queryFirstName = req.query.firstName;
+    queryLastName = req.query.lastName;
+    res.send(`Saved user Info => Name: ${queryFirstName} ${queryLastName}.`)
+})
+
+app.get('/show-user-info', (req, res) => {
+    console.log(req.query);
+    res.send(`Showing user Info => Name: ${queryFirstName} ${queryLastName}.`)
+})
+
+// Sending the concatenated movie list 
+app.get('/list-movies', (req, res) => {
+    let moviesString = favoriteMovieList.join(', ');
+    res.send(`Here is my movies string: ${moviesString}.`);
+})
+
+app.get('/add-movie', (req, res) => {
+    newMovie = req.query.movie;
+    favoriteMovieList.push(newMovie);
+    res.send(`${newMovie}`)
+})
+
+// Our port listener
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
